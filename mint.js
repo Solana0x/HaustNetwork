94750a07e18709637fb10963eac17f17a44ca28c
0xSolana <142747768+Solana0x@users.noreply.github.com>
2025-02-10 20:07:46 +0530

    Update mint.js

--- mint.js
+++ mint.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 const { ethers } = require("ethers");
 const fs = require("fs");
 const readline = require("readline");
+
 const NETWORK_RPC = "https://rpc-testnet.haust.app";
 const CHAIN_ID = 1523903251;
-const CONTRACT_ADDRESS = "0x6B3f185C4c9246c52acE736CA23170801D636c8E";
-const MAX_CONCURRENT = 1;
-const CONTRACT_ABI = [
+
+const CONTRACT_ADDRESS_1 = "0x6B3f185C4c9246c52acE736CA23170801D636c8E"; // NFT1
+const CONTRACT_ADDRESS_2 = "0x28e50a3632961dA179b2Afca4675714ea22E7BB7"; // NFT2
+
+const CONTRACT_ABI_1 = [
     {
         "inputs": [
             {
@@ -692,67 +695,783 @@ const CONTRACT_ABI = [
         "type": "function"
     }
 ];
+const CONTRACT_ABI_2 = [
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "initialOwner",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "ERC721EnumerableForbiddenBatchMint",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "sender",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "ERC721IncorrectOwner",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "operator",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "ERC721InsufficientApproval",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "approver",
+        "type": "address"
+      }
+    ],
+    "name": "ERC721InvalidApprover",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "operator",
+        "type": "address"
+      }
+    ],
+    "name": "ERC721InvalidOperator",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "ERC721InvalidOwner",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "receiver",
+        "type": "address"
+      }
+    ],
+    "name": "ERC721InvalidReceiver",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "sender",
+        "type": "address"
+      }
+    ],
+    "name": "ERC721InvalidSender",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "ERC721NonexistentToken",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "index",
+        "type": "uint256"
+      }
+    ],
+    "name": "ERC721OutOfBoundsIndex",
+    "type": "error"
+  },
+  {
+    "inputs": [],
+    "name": "EnforcedPause",
+    "type": "error"
+  },
+  {
+    "inputs": [],
+    "name": "ExpectedPause",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "OwnableInvalidOwner",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
+      }
+    ],
+    "name": "OwnableUnauthorizedAccount",
+    "type": "error"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "approved",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "Approval",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "operator",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "bool",
+        "name": "approved",
+        "type": "bool"
+      }
+    ],
+    "name": "ApprovalForAll",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "OwnershipTransferred",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
+      }
+    ],
+    "name": "Paused",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
+      }
+    ],
+    "name": "Unpaused",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "approve",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "getApproved",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      }
+    ],
+    "name": "getUserMintStatus",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "operator",
+        "type": "address"
+      }
+    ],
+    "name": "isApprovedForAll",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "name",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "ownerOf",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "pause",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "paused",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "renounceOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "safeMint",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "safeTransferFrom",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bytes",
+        "name": "data",
+        "type": "bytes"
+      }
+    ],
+    "name": "safeTransferFrom",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "operator",
+        "type": "address"
+      },
+      {
+        "internalType": "bool",
+        "name": "approved",
+        "type": "bool"
+      }
+    ],
+    "name": "setApprovalForAll",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes4",
+        "name": "interfaceId",
+        "type": "bytes4"
+      }
+    ],
+    "name": "supportsInterface",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "index",
+        "type": "uint256"
+      }
+    ],
+    "name": "tokenByIndex",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "index",
+        "type": "uint256"
+      }
+    ],
+    "name": "tokenOfOwnerByIndex",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "tokenURI",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "transferFrom",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "transferOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "unpause",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
+
+const MAX_CONCURRENT = 1;
+
 async function getPrivateKeys() {
-    try {
-        const fileStream = fs.createReadStream("key.txt");
-        const rl = readline.createInterface({
-            input: fileStream,
-            crlfDelay: Infinity
-        });
+  try {
+    const fileStream = fs.createReadStream("key.txt");
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
 
-        const privateKeys = [];
-        for await (const line of rl) {
-            const trimmedKey = line.trim();
-            if (trimmedKey.length === 64 || trimmedKey.length === 66) {
-                privateKeys.push(trimmedKey);
-            }
-        }
-        return privateKeys;
-    } catch (error) {
-        console.error("Error reading key.txt:", error);
-        process.exit(1);
+    const privateKeys = [];
+    for await (const line of rl) {
+      const trimmedKey = line.trim();
+      if (trimmedKey.length === 64 || trimmedKey.length === 66) {
+        privateKeys.push(trimmedKey);
+      }
     }
+    return privateKeys;
+  } catch (error) {
+    console.error("Error reading key.txt:", error);
+    process.exit(1);
+  }
 }
 
-async function mintNFT(privateKey, attempt = 1) {
-    try {
-        const provider = new ethers.JsonRpcProvider(NETWORK_RPC, CHAIN_ID);
-        const wallet = new ethers.Wallet(privateKey, provider);
-        const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, wallet);
+async function mintNFTs(privateKey, mintNFT1, attempt = 1) {
+  try {
+    const provider = new ethers.JsonRpcProvider(NETWORK_RPC, CHAIN_ID);
+    const wallet = new ethers.Wallet(privateKey, provider);
 
-        console.log(`Minting NFT from wallet: ${wallet.address} (Attempt ${attempt})`);
-        const gasLimit = 400000;
-        const gasPrice = ethers.parseUnits("20", "gwei");
-        const tx = await contract.safeMint({
-            gasLimit: gasLimit,
-            gasPrice: gasPrice
-        });
-        console.log(`Transaction sent for ${wallet.address}, waiting for confirmation...`);
-        const receipt = await tx.wait();
-
-        console.log(`✅ Transaction confirmed! TX Hash: ${receipt.hash}`);
-        console.log(`🔗 Explorer Link: https://explorer-testnet.haust.app/tx/${receipt.hash}`);
-    } catch (error) {
-        console.error(`❌ Error minting for wallet ${privateKey.substring(0, 6)}...`, error.message);
-        if (attempt < 3) {
-            console.log(`🔄 Retrying for ${privateKey.substring(0, 6)} (Attempt ${attempt + 1})...`);
-            return await mintNFT(privateKey, attempt + 1);
-        } else {
-            console.log(`🚫 Skipping ${privateKey.substring(0, 6)} after 3 failed attempts.`);
-        }
+    console.log(`\nMinting from wallet: ${wallet.address} (Attempt ${attempt})`);
+    const gasLimit = 33269;
+    const gasPrice = ethers.parseUnits("5", "gwei");
+    if (mintNFT1) {
+      const contract1 = new ethers.Contract(CONTRACT_ADDRESS_1, CONTRACT_ABI_1, wallet);
+      console.log(`  1) Minting from FIRST NFT contract (${CONTRACT_ADDRESS_1})...`);
+      const tx1 = await contract1.safeMint({ gasLimit, gasPrice });
+      console.log("     Transaction sent. Waiting for confirmation (NFT1)...");
+      const receipt1 = await tx1.wait();
+      console.log(`     ✅ First NFT minted! TX Hash: ${receipt1.hash}`);
+      console.log(`     🔗 Explorer Link: https://explorer-testnet.haust.app/tx/${receipt1.hash}`);
+    } else {
+      console.log("  Skipping NFT1 minting as per user choice.");
     }
-}
-async function processKeys() {
-    const privateKeys = await getPrivateKeys();
-    console.log(`🔹 Found ${privateKeys.length} private keys. Processing with ${MAX_CONCURRENT} concurrent threads.`);
+    const contract2 = new ethers.Contract(CONTRACT_ADDRESS_2, CONTRACT_ABI_2, wallet);
+    console.log(`  2) Minting from SECOND NFT contract (${CONTRACT_ADDRESS_2})...`);
+    const tx2 = await contract2.safeMint({ gasLimit, gasPrice });
+    console.log("     Transaction sent. Waiting for confirmation (NFT2)...");
+    const receipt2 = await tx2.wait();
+    console.log(`     ✅ Second NFT minted! TX Hash: ${receipt2.hash}`);
+    console.log(`     🔗 Explorer Link: https://explorer-testnet.haust.app/tx/${receipt2.hash}`);
 
-    let index = 0;
-    while (index < privateKeys.length) {
-        const batch = privateKeys.slice(index, index + MAX_CONCURRENT);
-        await Promise.all(batch.map(key => mintNFT(key)));
-
-        index += MAX_CONCURRENT;
+  } catch (error) {
+    console.error(`❌ Error minting for wallet ${privateKey.substring(0, 6)}...`, error.message);
+    if (attempt < 3) {
+      console.log(`🔄 Retrying for ${privateKey.substring(0, 6)} (Attempt ${attempt + 1})...`);
+      return await mintNFTs(privateKey, mintNFT1, attempt + 1);
+    } else {
+      console.log(`🚫 Skipping ${privateKey.substring(0, 6)} after 3 failed attempts.`);
     }
-    console.log("🎉 All transactions processed!");
+  }
 }
-processKeys();
+
+async function processKeys(mintNFT1) {
+  const privateKeys = await getPrivateKeys();
+  console.log(`\n🔹 Found ${privateKeys.length} private keys. Processing with ${MAX_CONCURRENT} concurrent threads.`);
+
+  let index = 0;
+  while (index < privateKeys.length) {
+    const batch = privateKeys.slice(index, index + MAX_CONCURRENT);
+    await Promise.all(batch.map(key => mintNFTs(key, mintNFT1)));
+
+    index += MAX_CONCURRENT;
+  }
+  console.log("\n🎉 All transactions processed!");
+}
+
+async function main() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question("Do you want to mint NFT1 as well? (yes/no): ", async (answer) => {
+    rl.close();
+
+    const userInput = answer.trim().toLowerCase();
+    const mintNFT1 = (userInput === "yes" || userInput === "y");
+    await processKeys(mintNFT1);
+    process.exit(0);
+  });
+}
+
+main();
